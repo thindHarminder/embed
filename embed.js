18aabd685c5cee122ee62c70f53042bd4d1148f0
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-02-16 09:06:16 -0800

    Update embed.js

--- embed.js
+++ embed.js
@@ -26,6 +26,16 @@ if (isElementVisible(webflowBadge)) {
   button.style.bottom = "24px";
 }
 
+function isElementVisible(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
 
 // Add an event listener to the button
 button.addEventListener("click", () => {
